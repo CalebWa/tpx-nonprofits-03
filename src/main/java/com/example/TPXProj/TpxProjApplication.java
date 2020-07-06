685c38593daa379d1b1627b7a346776f33381ed0
Caleb Warner <warner94@purdue.edu>
2020-07-06 17:14:10 -0400

    Much better html

--- src/main/java/com/example/TPXProj/TpxProjApplication.java
+++ src/main/java/com/example/TPXProj/TpxProjApplication.java
@@ -18,17 +18,42 @@ class HelloController {
 
 	@GetMapping("/")
 	String hello() {
-		return "<!DOCTYPE html>\n" +
-				"<html>\n" +
+		return "<html>\n" +
 				"<head>\n" +
-				"<title>tpx-nonprofits-03</title>\n" +
+				"\t<title> Nonprofit Homepage</title>\n" +
+				"\t<link rel=\"stylesheet\" href=\"style.css\">\n" +
 				"</head>\n" +
 				"<body>\n" +
-				"\n" +
-				"<h1>Nonprofits 03</h1>\n" +
-				"<p>Hello World!</p>\n" +
-				"\n" +
+				"\t<h1> This is where the header for the awareness page will be </h1>\n" +
+				"\t\t<img src=\"child.png\" />\n" +
+				"\t\t<p> Data invoking peoples pathos </p>\n" +
+				"\t\t<img src=\"puppies.jpg\"/>\n" +
+				"\t\t<p> Data invoking peoples ethos </p>\n" +
+				"\t\t<img src=\"food.jpg\"/>\n" +
+				"\t\t<p> Data invoking peoples logos \n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"\t<a href=\"Questionaire.html\">\n" +
+				"\t<button type=\"button\" onclick=\"Volunteer()\"> Match Me With A Nonprofit! </button>\n" +
+				"\t</a>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"\t<a href=\"Questionaire2.html\">\n" +
+				"\t<button type=\"button\" onclick=\"Nonprofit()\"> Register Your Nonprofit </button>\n" +
+				"\t</a>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"<br>\n" +
+				"\t<footer> Property of Nonprofit Team 3 </footer>\n" +
+				"\t\n" +
 				"</body>\n" +
-				"</html>";
+				"</html>\n";
 	}
 }
