133fe81838d85d83e6ff1b2cb252fb59a1eb0516
Caleb Warner <warner94@purdue.edu>
2020-07-08 15:17:01 -0400

    File path now correct

--- src/main/java/com/example/TPXProj/TpxProjApplication.java
+++ src/main/java/com/example/TPXProj/TpxProjApplication.java
@@ -23,7 +23,7 @@ class HelloController {
 	@GetMapping("/")
 	String indexPage() {
 	    try {
-            File indexPageFile = new File("index.html");
+            File indexPageFile = new File("src\\main\\java\\com\\example\\TPXProj\\index.html");
             Scanner scanner = new Scanner(indexPageFile);
             StringBuilder indexPage = new StringBuilder();
 
@@ -35,43 +35,6 @@ class HelloController {
         } catch (FileNotFoundException e) {
             System.out.println("File could not be found");
         }
-
-		return "<html>\n" +
-				"<head>\n" +
-				"\t<title> Nonprofit Homepage</title>\n" +
-				"\t<link rel=\"stylesheet\" href=\"style.css\">\n" +
-				"</head>\n" +
-				"<body>\n" +
-				"\t<h1> This is where the header for the awareness page will be </h1>\n" +
-				"\t\t<img src=\"child.png\" />\n" +
-				"\t\t<p> Data invoking peoples pathos </p>\n" +
-				"\t\t<img src=\"puppies.jpg\"/>\n" +
-				"\t\t<p> Data invoking peoples ethos </p>\n" +
-				"\t\t<img src=\"food.jpg\"/>\n" +
-				"\t\t<p> Data invoking peoples logos \n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"\t<a href=\"Questionaire.html\">\n" +
-				"\t<button type=\"button\" onclick=\"Volunteer()\"> Match Me With A Nonprofit! </button>\n" +
-				"\t</a>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"\t<a href=\"Questionaire2.html\">\n" +
-				"\t<button type=\"button\" onclick=\"Nonprofit()\"> Register Your Nonprofit </button>\n" +
-				"\t</a>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"<br>\n" +
-				"\t<footer> Property of Nonprofit Team 3 </footer>\n" +
-				"\t\n" +
-				"</body>\n" +
-				"</html>\n";
+        return "";
 	}
 }
