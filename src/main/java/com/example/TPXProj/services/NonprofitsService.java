f6085a24e0c89d1668b9d5cd86ce9543f2c85368
Caleb Warner <warner94@purdue.edu>
2020-07-15 11:23:00 -0400

    Testing post

--- src/main/java/com/example/TPXProj/services/NonprofitsService.java
+++ src/main/java/com/example/TPXProj/services/NonprofitsService.java
@@ -4,6 +4,8 @@ import com.example.TPXProj.models.Nonprofits;
 import com.example.TPXProj.repositories.NonprofitsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,11 @@ public class NonprofitsService {
     @GetMapping("/api/nonprofits")
     public Iterable<Nonprofits> findAllNonProfits() {
         return repository.findAll();
+    }
+
+    @PostMapping("/test")
+    public String testMethod(@RequestBody String requestBody) {
+        System.out.println(requestBody);
+        return "Hi, this is a test.";
     }
 }
