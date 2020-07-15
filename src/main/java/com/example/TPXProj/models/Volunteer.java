ab38da34aeae42ecf029693bd029a8129a188966
Caleb Warner <warner94@purdue.edu>
2020-07-15 15:54:51 -0400

    created FormService.java

--- src/main/java/com/example/TPXProj/models/Volunteer.java
+++ src/main/java/com/example/TPXProj/models/Volunteer.java
@@ -1,4 +1,11 @@
 package com.example.TPXProj.models;
 
+import java.util.ArrayList;
+
 public class Volunteer {
+    private Integer contribution;
+    private ArrayList<Integer> activities;
+    private ArrayList<Integer> skills;
+    private Integer availability;
+    private Integer location;
 }
--- src/main/java/com/example/TPXProj/services/DatabaseService.java
+++ src/main/java/com/example/TPXProj/services/DatabaseService.java
@@ -4,8 +4,6 @@ import com.example.TPXProj.models.Nonprofits;
 import com.example.TPXProj.repositories.NonprofitsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,17 +14,5 @@ public class DatabaseService {
     @GetMapping("/api/nonprofits")
     public Iterable<Nonprofits> findAllNonProfits() {
         return repository.findAll();
-    }
-
-    @PostMapping("/volunteer-results")
-    public String serviceVolunteer(@RequestBody String formString) {
-        System.out.println(formString);
-        return "This functionality is not yet working.";
-    }
-
-    @PostMapping("/nonprofit-confirmation")
-    public String serviceNonprofit(@RequestBody String formString) {
-        System.out.println(formString);
-        return "This functionality is not yet working.";
     }
 }
