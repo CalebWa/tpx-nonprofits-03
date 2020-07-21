b19f41b12e7bf83b609004e8013761a630ce11f7
Caleb Warner <warner94@purdue.edu>
2020-07-21 10:23:06 -0400

    Trying / instead of "

--- src/main/java/com/example/TPXProj/services/DatabaseService.java
+++ src/main/java/com/example/TPXProj/services/DatabaseService.java
@@ -39,7 +39,7 @@ public class DatabaseService {
 
     @PostMapping("/volunteer-submit.html")
     public String serviceVolunteer(@RequestBody String formString) {
-        return FileReader.readFile("src\\main\\webapp\\volunteer-submit.html");
+        return FileReader.readFile("src/main/webapp/volunteer-submit.html");
 //        VolunteerProcessor vp = new VolunteerProcessor(formString);
 //        return vp.output(repository);
     }
